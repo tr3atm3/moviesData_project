ea9c961350253a4aa2814ca7eb14996064ffa492
tr3atm3 <tttushar.789@gmail.com>
2024-03-07 22:00:37 +0530

    fetchApiStarter

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const fetchMoviesHandler = async () => {
+    try {
+      const dataJson = await fetch("https://swapi.dve/api/films");
+      const data = await dataJson.json();
+
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch {}
+  };
+  const dummyMovies = [
+    {
+      id: 1,
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
+    },
+    {
+      id: 2,
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
